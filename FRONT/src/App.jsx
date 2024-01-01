22364e2a5d7ddcf558546b88f2e4004aa033d219
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2024-01-01 08:40:15 +0800

    Refactor File paths

--- FRONT/src/App.jsx
+++ FRONT/src/App.jsx
@@ -1,12 +1,15 @@
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./Home/home.jsx";
-import Auth from "./Auth/auth.jsx";
+import Login from "./Auth/Login/login.jsx";
+import Signup from "./Auth/SignUp/signUp.jsx";
+import NotFound from "./NotFound/notFound.jsx";
 
 export default function App() {
 	return (
 		<Routes>
-			<Route index element={<Home />} />
-			<Route path="/*" element={<Auth />} />
+			<Route index element={<h1>To-Do Lists</h1>} />
+			<Route path="/login" element={<Login />} />
+			<Route path="/signup" element={<Signup />} />
+			<Route path="*" element={<NotFound />} />
 		</Routes>
 	);
 }
--- FRONT/src/NotFound/notFound.jsx
+++ FRONT/src/NotFound/notFound.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { Link } from "react-router-dom";
 
 export default function NotFound() {
 	const navigate = useNavigate();
