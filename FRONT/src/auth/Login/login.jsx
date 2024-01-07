0a4fb10b0cd7aab81b098520bc33ae40542193f5
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2024-01-07 08:26:20 +0800

    Refactor: Made separate files for long components

--- FRONT/src/auth/Login/login.jsx
+++ FRONT/src/auth/Login/login.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect, useContext } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 
+import LoginForm from "./LoginForm";
 import Connecting from "../../Components/Connecting";
 import ServerError from "../../Components/ServerError";
 
@@ -33,79 +34,10 @@ export default function Login() {
 	return status == "connecting" ? (
 		<Connecting />
 	) : status == "not-logged-in" ? (
-		<Form navigate={navigate} />
+		<LoginForm navigate={navigate} />
 	) : status == "server-error" ? (
 		<ServerError />
 	) : (
 		<h1>Unknown Error</h1>
-	);
-}
-
-function Form({ navigate }) {
-	const [form, setForm] = useState({ username: "", password: "" });
-
-	const [errorMessage, setErrorMessage] = useState("");
-
-	const handleUsernameChange = (e) => {
-		setForm((prev) => ({ ...prev, username: e.target.value }));
-	};
-	const handlePasswordChange = (e) => {
-		setForm((prev) => ({ ...prev, password: e.target.value }));
-	};
-
-	async function handleFormSubmit() {
-		try {
-			const response = await axios.post("/login", form);
-			setErrorMessage("");
-			navigate("/home");
-			// console.log(response);
-		} catch (err) {
-			console.error(err);
-			if (!err.response || !err.response.data) {
-				setErrorMessage("Unknown Error.");
-			} else {
-				setErrorMessage(err.response.data.msg);
-			}
-		}
-	}
-	return (
-		<>
-			<h1>Login</h1>
-			<hr />
-			<form
-				onSubmit={(e) => {
-					e.preventDefault();
-					handleFormSubmit();
-				}}
-			>
-				<label htmlFor="username">Username</label>
-				<br />
-				<input
-					type="text"
-					id="username"
-					value={form.username}
-					onChange={handleUsernameChange}
-					autoComplete="off"
-				/>
-				<br />
-				<label htmlFor="password">Password</label>
-				<br />
-				<input
-					type="password"
-					id="password"
-					value={form.password}
-					onChange={handlePasswordChange}
-					autoComplete="off"
-				/>
-				<br />
-				<button type="submit">Login</button>
-			</form>
-			<p style={{ color: "red" }}>{errorMessage}</p>
-			<br />
-			<hr />
-			<p>
-				Don't have an account? <Link to="/signup">Sign Up</Link>
-			</p>
-		</>
 	);
 }
