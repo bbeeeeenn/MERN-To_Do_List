8d2f6ce5aa4e85ac832ae2d5233f190842bf7447
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2024-01-08 19:25:39 +0800

    .

--- FRONT/src/auth/Login/login.jsx
+++ FRONT/src/auth/Login/login.jsx
@@ -14,6 +14,7 @@ export default function Login() {
 		(async () => {
 			try {
 				const response = await axios.get("/status");
+				console.log(response);
 				if (response.data.loggedIn) {
 					navigate("/home");
 				} else {
