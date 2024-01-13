584df9e7c9f8936389cf843436bbf9457107f04d
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2024-01-13 23:26:30 +0800

    .

--- FRONT/src/axiosConfig.js
+++ FRONT/src/axiosConfig.js
@@ -1,4 +1,7 @@
 import axios from "axios";
 
+const prod = import.meta.env.PROD;
 axios.defaults.withCredentials = true;
-axios.defaults.baseURL = import.meta.env.VITE_SERVER_BASE_URL;
+axios.defaults.baseURL = !prod
+	? import.meta.env.VITE_SERVER_BASE_URL_LOCAL
+	: VITE_SERVER_BASE_URL_PROD;
